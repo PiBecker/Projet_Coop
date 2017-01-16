a9ac051810af2c80d9d2e2bcb2b5d2c3d3e1bb65
Pi <pierrebecker57540@gmail.com>
2017-01-16 09:36:35 +0100

    app.js update

--- js/app.js
+++ js/app.js
@@ -1,14 +1,20 @@
 var app = angular.module("coop", ['ngResource']);
+var url_local = 'http://http://localhost/Projet_Coop/';
 app.constant('api', {'key': 'ea7303b5009143bea9c81082e12bac96', 'url': 'http://coop.api.netlor.fr/api'});
 
 app.service('TokenService', [function(){
-	this.token = '';
-	this.setToken = function(t){
-		this.token = t;
-	}
+	this.token = null;
+	this.setToken = function(t) {
+			if (localStorage.getItem('token') === undefined)
+					localStorage.setItem('token', t);
+			else
+					this.token = localStorage.getItem('token');
 
 	this.getToken = function(){
-		return this.token;
+		if (localStorage.getItem('token') === undefined)
+			return localStorage.getItem('token');
+		else
+			return null;
 	}
 }]);
 
@@ -30,20 +36,40 @@ app.config(['$httpProvider', 'api', function($httpProvider, api){
 
 app.factory("Member", ['$resource', 'api', function($resource, api){
 	return $resource(api.url+'/members/:id', {id: '@_id'}, {
-		update: {method: 'PUT'},
-		signin: {method: 'POST', url: api.url+'/members/signin'}
+		update: {
+			method: 'PUT'
+		},
+		signin: {
+			method: 'POST',
+			url: api.url+'/members/signin'
+		}
 	});
 }]);
 
-app.controller("StartController", ['$scope', 'Member', 'TokenService',function($scope, Member, TokenService){
+app.controller("StartController", ['$scope', 'Member', 'TokenService', function($scope, Member, TokenService) {
+    $scope.member = Member.signin({
+            email: 'toto@toto.fr',
+            password: 'toto',
+        },
+        function(m) {
+            $scope.member = m;
+            console.log($scope.member);
+            TokenService.setToken($scope.member.token);
+            $scope.member = Member.query(function(member) {
+                console.log(member);
+            })
+        },
+        function(e) {
+            console.log(e);
+        });
 
-	Member.signin({email: "toto2@coop.fr", password: 'toto'}, function(m){
-		$scope.member = m;
-		TokenService.setToken($scope.member.token);
-		$scope.members = Member.query(function(members){
-			console.log($scope.members);
-		});
-	});
+    $scope.members = Member.query(function(m) {
+            console.log(m);
+        },
+        function(error) {
+            console.log(error);
+        }
+    );
 
 	// $scope.newMember = new Member({
 	// 	fullname: "TOTO",
