f657a53e897938c086bc84384434686b740783cd
Pi <pierrebecker57540@gmail.com>
2017-01-16 09:21:54 +0100

    Initial Comm

---
+++ app.js
@@ -0,0 +1,80 @@
+var app = angular.module("coop", ['ngResource']);
+app.constant('api', {'key': 'ea7303b5009143bea9c81082e12bac96', 'url': 'http://coop.api.netlor.fr/api'});
+
+app.service('TokenService', [function(){
+	this.token = '';
+	this.setToken = function(t){
+		this.token = t;
+	}
+
+	this.getToken = function(){
+		return this.token;
+	}
+}]);
+
+app.config(['$httpProvider', 'api', function($httpProvider, api){
+	$httpProvider.defaults.headers.common.Authorization = 'Token token='+api.key;
+
+	$httpProvider.interceptors.push(['TokenService', function(TokenService){
+		return {
+			request: function(config){
+				var token = TokenService.getToken();
+				if(token != ""){
+					config.url += ((config.url.indexOf('?') >= 0) ? '&' : '?')
+	 	     							 +'token='+token;
+				}
+				return config;
+			}
+		};
+	}]);
+
+app.factory("Member", ['$resource', 'api', function($resource, api){
+	return $resource(api.url+'/members/:id', {id: '@_id'}, {
+		update: {method: 'PUT'},
+		signin: {method: 'POST', url: api.url+'/members/signin'}
+	});
+}]);
+
+app.controller("StartController", ['$scope', 'Member', 'TokenService',function($scope, Member, TokenService){
+
+	Member.signin({email: "toto2@coop.fr", password: 'toto'}, function(m){
+		$scope.member = m;
+		TokenService.setToken($scope.member.token);
+		$scope.members = Member.query(function(members){
+			console.log($scope.members);
+		});
+	});
+
+	// $scope.newMember = new Member({
+	// 	fullname: "TOTO",
+	// 	email: "toto2@coop.fr",
+	// 	password: "toto"
+	// });
+
+	// $scope.newMember.$save(function(m){
+	// 	console.log($scope.newMember);
+	// }, function(e){
+
+	// });
+
+/*$scope.member =	Member.save({
+		fullname: "TOTO",
+		email: "toto4@coop.fr",
+		password: "toto"
+	}, function(m){
+		$scope.member.$delete(function(success){
+			console.log(success);
+		});
+	}, function(e){
+		console.log($scope.newMember);
+	});
+
+	$scope.members = Member.query(
+		function(m){
+			console.log(m);
+		},
+		function(error){
+			console.log(error);
+		}
+	);*/
+}]);
