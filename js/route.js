4e2cdce3146c15a8921561edbf10d8a6fa02e768
v_reeb <victor.reeb@live.fr>
2017-02-02 05:09:15 +0100

    ajout routes, views

--- js/route.js
+++ js/route.js
@@ -2,12 +2,35 @@ var app = angular.module("coop");
 
 app.config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/', {
-            template: '',
-            controller: 'StartController'
+            templateUrl: './index.html',
         })
 
-        .when('/signin', {
-            templateUrl: './views/sign_in.html'
+        .when('/members', {
+            templateUrl: './views/members.html'
+        })
+
+        .when('/members/signin', {
+            templateUrl: './views/members/sign_in.html'
+        })
+
+        .when('/members/signup', {
+            templateUrl: './views/members/sign_up.html'
+        })
+
+        .when('/members/signout', {
+            templateUrl: './views/members/sign_out.html'
+        })
+
+        .when('/channels', {
+            templateUrl: './views/channels.html'
+        })
+
+        .when('/channels/new', {
+            templateUrl: './views/channels/new.html'
+        })
+
+        .when('/channels/:id', {
+            templateUrl: './views/channels/show.html'
         })
 
         .otherwise({
